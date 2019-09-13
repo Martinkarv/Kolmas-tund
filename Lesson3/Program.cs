dd673b4ffadb16209c6b0f6c3d103bd8aa27ab59
Opilane TTHK <opilane@tthk.ee>
2019-09-13 18:18:59 +0300

    first commit

---
+++ Lesson3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+
+            //Arithmetic Operators
+            //Multiple Line Comment
+            /*Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);*/
+
+            //Console.WriteLine(x = y);
+            //Console.WriteLine(x += y);
+            //Console.WriteLine(x -= y);
+            //Console.WriteLine(x *= y);
+            //Console.WriteLine(x % y);
+
+            //Comparison Operators
+
+            /*Console.WriteLine(x == y);
+            Console.WriteLine(x != y);
+            Console.WriteLine(x > y);
+            Console.WriteLine(x < y);
+            Console.WriteLine(x >= y);
+            Console.WriteLine(x <= y);*/
+
+
+            //Logical Operators
+            //&& - AND
+            //|| - OR
+            //Console.WriteLine(x != y && y == 2);
+            Console.WriteLine(x != y && y == 5);
+            Console.WriteLine(x != y || y == 2);
+            Console.WriteLine(x != y || y == 5);
+            Console.WriteLine(!(y == 10));
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
